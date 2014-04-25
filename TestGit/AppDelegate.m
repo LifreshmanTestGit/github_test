b85989abe61221c17b3c78d154e8dc03c167f1b6
lifreshman <lifreshman@126.com>
2014-04-25 22:46:33 +0800

    TestGit
    add nslog("2")

--- TestGit/AppDelegate.m
+++ TestGit/AppDelegate.m
@@ -15,6 +15,9 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+    
+    NSLog(@"2");
+    
     NSLog(@"TestBranch top");
     NSLog(@"master middle");
     NSLog(@"master bottom");
