fec158012499991285c8e6ec756cd26a9afc96f7
lifreshman <lifreshman@126.com>
2014-04-25 22:32:33 +0800

    TestGit
    test TestBranch

--- TestGit/AppDelegate.m
+++ TestGit/AppDelegate.m
@@ -15,7 +15,7 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
-    
+    NSLog(@"TestBranch top");
     NSLog(@"master middle");
     [self.window makeKeyAndVisible];
     return YES;
