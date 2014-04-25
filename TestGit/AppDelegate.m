a75ef18a474732c99cdb99a1439cdc220a9b3fd6
lifreshman <lifreshman@126.com>
2014-04-25 21:50:33 +0800

    TestGit
    Test commit

--- TestGit/AppDelegate.m
+++ TestGit/AppDelegate.m
@@ -15,6 +15,8 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+    
+    NSLog(@"master middle");
     [self.window makeKeyAndVisible];
     return YES;
 }
