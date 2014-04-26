22ea86c3e0f39bb345a50621c6e12723bb84a3ec
lifreshman <lifreshman@126.com>
2014-04-26 17:01:07 +0800

    增加一个github远程ViewController

---
+++ TestGit/TestRemoteVC.h
@@ -0,0 +1,13 @@
+//
+//  TestRemoteVC.h
+//  TestGit
+//
+//  Created by li xinsheng on 14-4-26.
+//  Copyright (c) 2014年 li xinsheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestRemoteVC : UIViewController
+
+@end
---
+++ TestGit/TestRemoteVC.m
@@ -0,0 +1,39 @@
+//
+//  TestRemoteVC.m
+//  TestGit
+//
+//  Created by li xinsheng on 14-4-26.
+//  Copyright (c) 2014年 li xinsheng. All rights reserved.
+//
+
+#import "TestRemoteVC.h"
+
+@interface TestRemoteVC ()
+
+@end
+
+@implementation TestRemoteVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
